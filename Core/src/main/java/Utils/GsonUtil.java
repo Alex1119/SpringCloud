867044657e2910a8d732ea7abc6b2e51ef4b928e
Alex Xing <916000345@qq.com>
2017-12-22 16:48:21 +0800

    modify config

---
+++ Core/src/main/java/Utils/GsonUtil.java
@@ -0,0 +1,78 @@
+package Utils;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GsonUtil {
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    private static final Gson gson = new GsonBuilder().serializeNulls()
+            .enableComplexMapKeySerialization().setDateFormat(DATE_FORMAT).create();
+
+    /**
+     * 转成Json字符串
+     */
+    public static String toJson(Object object) {
+        return gson.toJson(object);
+    }
+
+    /**
+     * Json转Java对象
+     */
+    public static <T> T fromJson(String json, Class<T> clz) {
+        return gson.fromJson(json, clz);
+    }
+
+    /**
+     * Json转Java对象
+     */
+    public static <T> T fromJson(String json, Type type) {
+        return gson.fromJson(json, type);
+    }
+
+    /**
+     * Json转List集合
+     */
+    public static <T> List<T> jsonToList(String json, Class<T> clz) {
+        Type type = new TypeToken<List<T>>() {
+        }.getType();
+        return gson.fromJson(json, type);
+    }
+
+    /**
+     * Json转List集合,遇到解析不了的，就使用这个
+     */
+    public static <T> List<T> fromJsonList(String json, Class<T> cls) {
+        List<T> mList = new ArrayList<T>();
+        JsonArray array = new JsonParser().parse(json).getAsJsonArray();
+        Gson mGson = new Gson();
+        for (final JsonElement elem : array) {
+            mList.add(mGson.fromJson(elem, cls));
+        }
+        return mList;
+    }
+
+    /**
+     * Json转换成Map的List集合对象
+     */
+    public static <T> List<Map<String, T>> toListMap(String json, Class<T> clz) {
+        Type type = new TypeToken<List<Map<String, T>>>() {
+        }.getType();
+        return gson.fromJson(json, type);
+    }
+
+    /**
+     * Json转Map对象
+     */
+    public static <T> Map<String, T> toMap(String json, Class<T> clz) {
+        Type type = new TypeToken<Map<String, T>>() {
+        }.getType();
+        return gson.fromJson(json, type);
+    }
+}
